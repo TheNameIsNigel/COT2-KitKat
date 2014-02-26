1a6bb6c011ad18656ceb06160108ffdcd279f33b
Drew Walton <drewwalton19216801@gmail.com>
2014-02-26 11:20:38 -0700

    recovery: fix relative paths

--- recovery.c
+++ recovery.c
@@ -825,6 +825,7 @@ int run_script_file(void) {
     while (fgets(script_line, SCRIPT_COMMAND_SIZE, fp) != NULL && ret_val == 0) {
       cindex = 0;
       line_len = strlen(script_line);
+      printf("ORS command: %s\n", script_line);
       //if (line_len > 2)
       //continue; // there's a blank line at the end of the file, we're done!
       //ui_print("script line: '%s'\n", script_line);
@@ -849,16 +850,30 @@ int run_script_file(void) {
 	LOGI("value is: '%s'\n", value);
       } else {
 	strncpy(command, script_line, line_len - remove_nl + 1);
-	ui_print("command is: '%s' and there is no value\n", command);
+	LOGI("command is: '%s' and there is no value\n", command);
       }
       if (strcmp(command, "install") == 0) {
 	// Install zip -- ToDo : Need to clean this shit up, it's redundant and I know it can be written better
-	ensure_path_mounted(get_primary_storage_path());
-	ui_print("Installing zip file '%s'\n", value);
-	ret_val = install_zip(value);
-	if (ret_val != INSTALL_SUCCESS) {
-	  LOGE("Error installing zip file '%s'\n", value);
-	  ret_val = 1;
+	char full_path[SCRIPT_COMMAND_SIZE];
+	if (value[0] != '/') {
+	  //relative path given
+	  sprintf(full_path, "%s/%s", get_primary_storage_path(), value);
+	  ensure_path_mounted(full_path);
+	  ui_print("Installing zip file '%s'\n", full_path);
+	  ret_val = install_zip(full_path);
+	  if (ret_val != INSTALL_SUCCESS) {
+	    LOGE("Error installing '%'\n", full_path);
+	    ret_val = 1;
+	  }
+	} else {
+	  // Full path given
+	  ensure_path_mounted(get_primary_storage_path());
+	  ui_print("Installing zip file '%s'\n", value);
+	  ret_val = install_zip(value);
+	  if (ret_val != INSTALL_SUCCESS) {
+	    LOGE("Error installing '%s'\n", value);
+	    ret_val = 1;
+	  }
 	}
       } else if (strcmp(command, "wipe") == 0) {
 	// Wipe -- ToDo: Make this use the same wipe functionality as normal wipes
